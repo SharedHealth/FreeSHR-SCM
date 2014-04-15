eab55c9cb0ea8dcbdd25340dabb8a758a760d544
Balaji Narain <narainbalaji@gmail.com>
2014-04-15 14:10:54 +0530

    BalajiN | Waiting for the service to finish starting up before executing docker

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,6 +13,14 @@ yum install -y docker-io
 #start the docker service
 service docker start
 
+#Wait for the service to be started
+while [ ! -e /var/run/docker.sock ]
+do
+  sleep 2
+done
+
+echo "Pulling centos"
+
 #install pull the centos base image
 docker pull centos
 
