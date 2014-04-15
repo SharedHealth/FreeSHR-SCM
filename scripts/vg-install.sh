a0914dd066c70af26b940b320ac7ea86b9c657f8
Balaji Narain <narainbalaji@gmail.com>
2014-04-16 00:02:16 +0530

    BalajiN | Updating vagrant installation

--- scripts/vg-install.sh
+++ scripts/vg-install.sh
@@ -3,5 +3,6 @@
 sudo su
 
 #Install the rpms over here
-rpm -ivh /vagrant/dist/shr-scm-deps-0.1-1.noarch.rpm
+yum install -y /vagrant/dist/shr-scm-repo-0.1-1.noarch.rpm
+yum install -y /vagrant/dist/shr-scm-deps-0.1-1.noarch.rpm
 yum install -y /vagrant/dist/shr-scm-install-0.1-1.noarch.rpm
