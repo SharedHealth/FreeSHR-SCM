af78dbcc338b804aef9ac21d6c5ce93ea1c97c81
Balaji Narain <narainbalaji@gmail.com>
2014-04-15 13:48:12 +0530

    BalajiN | fixing bugs with vagrant up

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+#sudo
+sudo su
+
 #install docker
 rpm -ivh /vagrant/lib/xz.rpm
 rpm -ivh /vagrant/lib/bridge-utils.rpm
 rpm -ivh /vagrant/lib/epel.rpm
 rpm -ivh /vagrant/lib/remi.rpm
 yum install -y docker-io
+
+#start the docker service
+service docker start
 
 #install pull the centos base image
 docker pull centos
