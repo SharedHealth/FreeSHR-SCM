76fff7e4e05e24de4a758255c31d08f1dc2b6186
Balaji Narain <balajin@thoughtworks.com>
2014-06-30 22:28:59 +0530

    BalajiN | Updating rpm

--- scripts/rpm/clone.sh
+++ scripts/rpm/clone.sh
@@ -2,4 +2,6 @@
 if [[ ! -e /playbooks ]]; then
 	mkdir /playbooks
 fi
+#Ensure that git can handle large files
+git config --global http.postBuffer 524288000
 (cd /playbooks && git clone https://github.com/SharedHealth/FreeSHR-Playbooks.git)
