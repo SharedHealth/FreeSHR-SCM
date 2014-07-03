4987046e4321285db8ec0b7d18dc1439ada91ad8
Balaji Narain <balajin@thoughtworks.com>
2014-07-03 10:55:30 +0530

    BalajiN | Also cloning the playbooks for bahmni

--- scripts/rpm/clone.sh
+++ scripts/rpm/clone.sh
@@ -5,3 +5,4 @@ fi
 #Ensure that git can handle large files
 git config --global http.postBuffer 524288000
 (cd /playbooks && git clone https://github.com/SharedHealth/FreeSHR-Playbooks.git)
+(cd /playbooks && git clone https://github.com/SharedHealth/FreeSHR-Bahmni-Playbooks.git)
